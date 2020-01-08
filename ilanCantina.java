6c9e8a7aef32937cda2c6dbde880b1ed2e4b17a6
Ilan Hascal <ihascal@brandeis.edu>
2020-01-08 13:43:52 -0500

    Initialized the repo and added client code..currently only accepting user input

---
+++ ilanCantina.java
@@ -0,0 +1,23 @@
+
+/*
+    Author : Ilan Hascal 
+    Description:
+    Program is going to read input form stdin and print correspoinding relevant output from given JSON file (SystemViewController)
+*/
+
+import java.util.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ilanCantina {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String a = "";
+        while (!a.equals("q")) {
+            System.out.println("Please enter the selecor you would like to use (Enter q to quit): ");
+            a = console.nextLine();
+
+        }
+
+    }
+}
