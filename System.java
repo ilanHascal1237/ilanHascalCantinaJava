cb913d30e26c5c1705583e48385fb4b808797e07
Ilan Hascal <ihascal@brandeis.edu>
2020-01-16 00:08:55 -0500

    Able to fetch and write to a file based off the gir curl..added scanner and imported classes

---
+++ System.java
@@ -0,0 +1,46 @@
+
+/**
+ * @author ILAN HASCAL Ilanhascal1237@gmail.com
+ * 
+ *         Program fetches SystemViewController.json , parses and then allows
+ *         for stdin commands form the terminal
+ * 
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class readsysview {
+    public static void main(String[] args) {
+        // url from gitHub
+        String curl = "https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json";
+        String filename = "systemView.json";
+        BufferedReader reader = readFromGit(curl);
+        if (reader != null) {
+            try {
+                String line = null;
+                BufferedWriter filewriter = new BufferedWriter(new FileWriter(filename));
+                while ((line = reader.readLine()) != null) {
+                    filewriter.append(line + "\n");
+                }
+                filewriter.close();
+            } catch (IOException e) {
+                System.out.println("No JSON file found.");
+            }
+
+        }
+
+    }
+}
