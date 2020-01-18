2b3b39f3884ce130387681c96eef36d0b7517bea
Ilan Hascal <ihascal@brandeis.edu>
2020-01-18 13:40:13 -0500

    added javaDocs...commented..still need to work on recursion case and compiund selector case..abe to write to file properly and parse json

--- System.java
+++ System.java
@@ -61,6 +61,10 @@ public class readsysview {
     /*
      * This method is going to identify what type of identifier the user inputs
      */
+    /**
+     * @param line
+     * @param sys
+     */
     public static void select(String line, JSONObject sys) {
         if (line.contains(".")) {
             // checking for classNames
@@ -83,6 +87,10 @@ public class readsysview {
      * Method uses the public gutHub link to fetch its contents
      *
      */
+    /**
+     * @param curl
+     * @return
+     */
     public static BufferedReader readFromGit(String curl) {
         try {
             URL url = new URL(curl);
@@ -96,25 +104,95 @@ public class readsysview {
 
     }
 
+    /**
+     * @param line
+     * @param sys
+     * @param pattern
+     */
     public static void selectClasses(String line, JSONObject sys, String pattern) {
         System.out.println("MATCHING VIEWS: " + recurseClasses(line, sys, pattern));
     }
 
+    /**
+     * @param line
+     * @param sys
+     * @param pattern
+     * @return
+     */
     public static int recurseClasses(String line, JSONObject sys, String pattern) {
-        int viewsfound = 0;
-        for (Object o : sys.keySet()) {
-            if (sys.get(o) instanceof JSONArray) { // loop case
-                JSONArray ja = (JSONArray) sys.get(o);
-                for (Object n : ja.toArray()) {
-                    if (n instanceof JSONObject) {
-                        if (checkJSONObject(line, (JSONObject) n, pattern))
-                            viewsfound++;
+        int viewsfound = 0;// keep track of how many we hace seen
+        for (Object o : sys.keySet()) { // iterate through the keys
+            if (sys.get(o) instanceof JSONArray) { // loop case, for an array
+                JSONArray ja = (JSONArray) sys.get(o); // get the object
+                for (Object n : ja.toArray()) {// iterate through the array
+                    if (n instanceof JSONObject) {// checking if it is an object
+                        if (checkJSONObject(line, (JSONObject) n, pattern)) // call checkJSON for contents
+                            viewsfound++; // if ture,incerement views
                         viewsfound += recurseClasses(line, (JSONObject) n, pattern);// keep searching
                     }
                 }
+            } else if (sys.get(o) instanceof JSONObject) {// single case
+                if (checkJSONObject(line, (JSONObject) sys.get(o), pattern))
+                    viewsfound++;
+                viewsfound += recurseClasses(line, (JSONObject) sys.get(o), pattern);
             }
         }
         return viewsfound;
+    }
+
+    /**
+     * @param line
+     * @param jo
+     * @param pattern
+     * @return
+     */
+    public static boolean checkJSONObject(String line, JSONObject jo, String pattern) {
+        if (jo.containsKey(pattern)) {// if we see a familiar patter, error check
+            if (pattern != "classNames") {
+                if (jo.get(pattern).equals(line)) {
+                    System.out.println("VIEW: " + jo.get("class") + "\n" + "JSON: ");
+                    for (Object key : jo.keySet()) {// iterate through the keys of the object we just found based off of
+                                                    // pattern
+                        if (!key.equals(pattern)) {
+                            System.out.println("	" + jo.get(key));
+                        }
+                    }
+                    System.out.println();
+                    return true;
+                }
+            } else {
+                return checkJSONArray((JSONArray) jo.get(pattern), jo.get("class"), line);
+            }
+
+        }
+        return false;
+    }
+
+    /**
+     * @param jarr
+     * @param viewname
+     * @param line
+     * @return
+     */
+    public static boolean checkJSONArray(JSONArray jarr, Object viewname, String line) {
+        if (jarr.contains(line)) { // seeing if they correpespons (pattern and the line we pass)
+            System.out.println("VIEW: " + viewname + "\n" + "JSON: " + "\n" + "	" + jarr + "\n");
+            return true;
+        }
+        return false;
+    }
+
+    public static JSONObject parseJson(String filename) {
+        try {
+            FileReader reader = new FileReader(new File(filename));
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonobject = (JSONObject) jsonParser.parse(reader);
+            return jsonobject;
+        } catch (Exception e) {
+            System.out.println("Could not parse json.");
+            return null;
+        }
+
     }
 
 }
