3db725142a35b28c03716226382a54b54c14bdf7
Ilan Hascal <ihascal@brandeis.edu>
2020-01-20 16:54:49 -0500

    added method domunetation,removed most of the body comments

--- System.java
+++ System.java
@@ -23,9 +23,10 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class System {
-
+    /*
+    
+    */
     public static void main(String[] args) {
-        // url from gitHub
         String curl = "https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json";
         String filename = "systemView.json";
         writeJsonFile(curl, filename);
@@ -34,7 +35,7 @@ public class System {
     }
 
     /**
-     * @param sys
+     * @param sys - JSON object
      */
     public static void repl(JSONObject sys) {
         Scanner consolereader = new Scanner(System.in);
@@ -49,21 +50,21 @@ public class System {
     }
 
     /**
-     * @param line - is reading from the scanner
+     * 
+     * Modular methos works for each selctor either
+     * class,classNames,identifierclass. Aftwe
+     * 
+     * @param line - console input
      * @param sys  - parsed josn object that is returned from parseJson
      */
     public static void select(String line, JSONObject sys) {
         if (line.contains(".")) {
-            // checking for classNames
-            // then removing the identifier
             line = line.replace(".", "");
             selectClasses(line, sys, "classNames");
         } else if (line.contains("#")) {
-            // checking for an identifier
             line = line.replace("#", "");
             selectClasses(line, sys, "identifier");
         } else {
-            // otherwise we know it is a class
             selectClasses(line, sys, "class");
         }
 
@@ -80,20 +81,29 @@ public class System {
 
     /**
      * @param line    - line the scanner is reading
-     * @param sys     -
+     * @param sys     - overall json object
      * @param pattern - is the selector we are using
      * @return - number of iterations
+     * 
+     *         For each key in the JSON object, if its values is an array, we know
+     *         that we need to examine each element in the array. We then cast all
+     *         generic java objects into JSON objects.Then given a JSON object, we
+     *         call checkJSONObject. If this method returns true, we increment the
+     *         number of views we have seen.
+     * 
+     * 
      */
     public static int recurseClasses(String line, JSONObject sys, String pattern) {
-        int viewsfound = 0;// keep track of how many we hace seen
-        for (Object o : sys.keySet()) { // iterate through the keys
+        int viewsfound = 0;
+        for (Object o : sys.keySet()) {
             if (sys.get(o) instanceof JSONArray) { // loop case, for an array
-                JSONArray ja = (JSONArray) sys.get(o); // get the object
-                for (Object n : ja.toArray()) {// iterate through the array
-                    if (n instanceof JSONObject) {// checking if it is an object
+                JSONArray ja = (JSONArray) sys.get(o);
+                for (Object n : ja.toArray()) {
+                    if (n instanceof JSONObject) {
                         if (checkJSONObject(line, (JSONObject) n, pattern)) // call checkJSON for contents
                             viewsfound++; // if ture,incerement views
-                        viewsfound += recurseClasses(line, (JSONObject) n, pattern);// keep searching
+                        viewsfound += recurseClasses(line, (JSONObject) n, pattern);// keep searching and increment for
+                                                                                    // nested JSON
                     }
                 }
             } else if (sys.get(o) instanceof JSONObject) {// single case
@@ -106,35 +116,24 @@ public class System {
     }
 
     /**
-     * @param curl - is the gitHub link tot he public repo
-     * @return - a BufferReader that can traverse the JSON
-     */
-    public static BufferedReader readFromGit(String curl) {
-        try {
-            URL url = new URL(curl);
-            URLConnection uc;
-            uc = url.openConnection();// fethcing
-            return new BufferedReader(new InputStreamReader(uc.getInputStream()));
-        } catch (IOException e) {
-            System.out.println("No Github resource found.");
-            return null;
-        }
-
-    }
-
-    /**
-     * @param line    - line we are reading form the scanner
+     * @param line    - console inout
      * @param jo      - json object
-     * @param pattern - is the selector we are using
+     * @param pattern - selector we are using
      * @return - wether we are successfuly able to find the view
+     * 
+     *         After examining the JSON file, classNames is most of the time
+     *         associated with an array so we call checkJSONArray() on ln 144. If it
+     *         is either of the other 2 selectors, we compare the value assoicated
+     *         with the selector is equal to the console input. We then print and
+     *         return true.
+     * 
      */
     public static boolean checkJSONObject(String line, JSONObject jo, String pattern) {
-        if (jo.containsKey(pattern)) {// if we see a familiar patter, error check
+        if (jo.containsKey(pattern)) {
             if (pattern != "classNames") {
                 if (jo.get(pattern).equals(line)) {
                     System.out.println("VIEW: " + jo.get("class") + "\n" + "JSON: ");
-                    for (Object key : jo.keySet()) {// iterate through the keys of the object we just found based off of
-                                                    // pattern
+                    for (Object key : jo.keySet()) {
                         if (!key.equals(pattern)) {
                             System.out.println("	" + jo.get(key));
                         }
@@ -151,13 +150,17 @@ public class System {
     }
 
     /**
+     * Method checks if console input your searching for matches the JSON array,
+     * returns true if so.
+     * 
      * @param jarr     - is the json array within the object
      * @param viewname - is the type of selector we are woking with
-     * @param line     - is reading form the scanner
+     * @param line     - Console input
      * @return - wether or not we have found the right view
+     * 
      */
     public static boolean checkJSONArray(JSONArray jarr, Object viewname, String line) {
-        if (jarr.contains(line)) { // seeing if they correpespons (pattern and the line we pass)
+        if (jarr.contains(line)) {
             System.out.println("VIEW: " + viewname + "\n" + "JSON: " + "\n" + "	" + jarr + "\n");
             return true;
         }
@@ -182,14 +185,31 @@ public class System {
     }
 
     /**
+     * @param curl - is the gitHub link tot he public repo
+     * @return - a BufferReader that can traverse the JSON
+     */
+    public static BufferedReader readFromGit(String curl) {
+        try {
+            URL url = new URL(curl);
+            URLConnection uc;
+            uc = url.openConnection();// fethcing
+            return new BufferedReader(new InputStreamReader(uc.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("No Github resource found.");
+            return null;
+        }
+
+    }
+
+    /***
      * @param curl     - url
      * @param filename - name of file
      * @return - return the parse JSON
+     * 
      */
     public static void writeJsonFile(String curl, String filename) {
         BufferedReader reader = readFromGit(curl);
         if (reader != null) {
-            // try/catch to sucessfully check if writing to json is working
             try {
                 String line = null;
                 BufferedWriter filewriter = new BufferedWriter(new FileWriter(filename));
@@ -203,55 +223,6 @@ public class System {
 
         }
 
-    }
-
-    public static void compundSelectFromClassNames(String viewname, String id, JSONObject sys) {
-        System.out.println("MATCHING VIEWS: " + recurseClassesCompound(viewname, id, "", sys));
-    }
-
-    public static void compoundSelectFromIdentifier(String viewname, String classname, JSONObject sys) {
-        System.out.println("MATCHING VIEWS: " + recurseClassesCompound(viewname, classname, "", sys));
-    }
-
-    public static int recurseClassesCompound(String viewname, String classname, String pattern, JSONObject sys) {
-        int viewsfound = 0;
-        for (Object o : sys.keySet()) {
-            if (sys.get(o) instanceof JSONArray) {
-                JSONArray ja = (JSONArray) sys.get(o);
-                for (Object n : ja.toArray()) {
-                    if (n instanceof JSONObject) {
-                        if (checkJSONObjectCompound(viewname, classname, pattern, (JSONObject) n))
-                            viewsfound++;
-                        viewsfound += recurseClassesCompound(viewname, classname, pattern, (JSONObject) n);
-                    }
-                }
-            } else if (sys.get(o) instanceof JSONObject) {
-                if (checkJSONObjectCompound(viewname, classname, pattern, (JSONObject) sys.get(o)))
-                    viewsfound++;
-                viewsfound += recurseClassesCompound(viewname, classname, pattern, (JSONObject) sys.get(o));
-            }
-        }
-        return viewsfound;
-    }
-
-    public static boolean checkJSONObjectCompound(String viewname, String filter, String pattern, JSONObject sys) {
-        if (sys.containsKey("class")) {
-            System.out.println("CLASS");
-            if (sys.containsKey(pattern)) {
-                System.out.println("PATTERN");
-                if (sys.get("class").equals(viewname)) {
-                    System.out.println("CORRECT CLASS");
-                    if (sys.get(pattern).equals(filter)) {
-                        System.out.println("CORRECT SECOND ARG");
-                        System.out.println("VIEW: " + viewname + "\n" + "JSON: ");
-                        System.out.println(sys);
-                        System.out.println();
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
     }
 
 }
